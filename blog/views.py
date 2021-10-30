c4e363459afd01520a3f7f95edabef26db485908
boom210232 <chonchanok.che@ku.th>
2021-10-30 20:35:30 +0700

    add dummy for result

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/blog_index.html',)
+    return render(request, 'blog/blog_index.html', )
+
 
 def about(request):
     return HttpResponse("This is a test of Django framework")
+
+
+def result_demo(request):
+    return render(request, 'blog/show_detail.html', )
