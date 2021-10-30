c5dace67bafb553d433c59014e897b30313c54dc
boom210232 <chonchanok.che@ku.th>
2021-10-30 20:29:45 +0700

    initation for search function

--- blog/models.py
+++ blog/models.py
@@ -1,45 +1,49 @@
-from django.db import models
 import asyncio
 
 import kitsu
-# Create your models here.
-class Blog():
 
-    def __init__(self):
-        self.client = kitsu.Client()
 
-    def start(self):
-        while True:
-            loop = asyncio.get_event_loop()
-            # loop.create_task(anime_search(input('Insert an anime name: ')))
-            loop.run_until_complete(self.anime_search(input('Insert an anime name: ')))
+client = kitsu.Client()
 
-    async def anime_search(self, query):
-        entries = await self.client.search('anime', query, limit=5)
-        if not entries:
-            print(f'No entries found for "{query}"')
-            return
 
-        for i, anime in enumerate(entries, 1):
-            print(f'\n{i}. {anime.title}:')
-            print('---> Sub-Type:', anime.subtype)
-            print('---> Status:', anime.status)
-            print('---> Synopsis:\n' + anime.synopsis)
-            print('---> Episodes:', anime.episode_count)
-            print('---> Age Rating:', anime.age_rating_guide)
-            print('---> Ranking:')
-            print('-> Popularity:', anime.popularity_rank)
-            print('-> Rating:', anime.rating_rank)
+async def anime_search(query):
+    entries = await client.search('anime', query, limit=1)
+    if not entries:
+        print(f'No entries found for "{query}"')
+        return
 
-            if anime.started_at:
-                print('---> Started At:', anime.started_at.strftime('%Y-%m-%d'))
-            if anime.ended_at:
-                print('---> Ended At:', anime.ended_at.strftime('%Y-%m-%d'))
+    for i, anime in enumerate(entries, 1):
+        print(f'\n{i}. {anime.title}:')
+        print('---> Sub-Type:', anime.subtype)
+        print('---> Status:', anime.status)
+        print('---> Synopsis:\n' + anime.synopsis)
+        print('---> Episodes:', anime.episode_count)
+        print('---> Age Rating:', anime.age_rating_guide)
+        print('---> Ranking:')
+        print('-> Popularity:', anime.popularity_rank)
+        print('-> Rating:', anime.rating_rank)
 
-            streaming_links = await self.client.fetch_anime_streaming_links(anime)
-            if streaming_links:
-                print('---> Streaming Links:')
-                for link in streaming_links:
-                    print(f'-> {link.title}: {link.url}')
+        if anime.started_at:
+            print('---> Started At:', anime.started_at.strftime('%Y-%m-%d'))
+        if anime.ended_at:
+            print('---> Ended At:', anime.ended_at.strftime('%Y-%m-%d'))
 
-            print('---> Kitsu Page:', anime.url)
+        streaming_links = await client.fetch_anime_streaming_links(anime)
+        if streaming_links:
+            print('---> Streaming Links:')
+            for link in streaming_links:
+                print(f'-> {link.title}: {link.url}')
+
+        print('---> Kitsu Page:', anime.url)
+
+
+
+
+def api_uses(anime):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(anime_search(str(anime)))
+
+
+api_uses('Pokemon Introductory Recap')
+
+
