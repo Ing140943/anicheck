68e9e0d7671628eba8cf2f692dc1b688984b127b
borrabeam <bannakorn.s@ku.th>
2021-11-03 23:49:50 +0700

    Update views, models and html.

--- blog/models.py
+++ blog/models.py
@@ -20,42 +20,48 @@ async def anime_search(query):
             for link in streaming_links:
                 s_links[link.title] = link.url
         
-        print(anime.title+"l")
-        url = "https://kitsu.io/api/edge/anime?filter[text]=Naruto"
-        response = requests.get(url)
-        l = response.json()['data']
-        poster_image_link = ""
-        for info in l:
-            if info['attributes']['titles']['en'] == "Naruto":
-                poster_image_link = info['attributes']['posterImage']['original']
-                break
-        print(anime.title)
+        # print(anime.title+"l")
+        
+        # url = 
+        anime_title = anime.title
+
+        use_url = f"https://kitsu.io/api/edge/anime?filter[text]={anime_title}"
+
+      
+        response = requests.get(use_url)
+        # 
+        # l = response.json()['data']
+        # poster_image_link = ""
+        # for info in l:
+# 'titles']['titles'][        #     if "DragonBall" in info['attributes']['titles']['en']:
+        #         poster_image_link = info['attributes']['posterImage']['original']
+                # break
+
         demo[anime.title] = {
             "sub-type" : anime.subtype,
             "status" : anime.status,
             "synopsis" : anime.synopsis,
             "episode" : anime.episode_count,
             "age-rating" : anime.age_rating_guide,
-            "ranking" 
             "popularity" : anime.popularity_rank,
             "rating" : anime.rating_rank,
-            "start_at" : anime.started_at.strftime('%Y-%m-%d'),
-            "ended_at" : anime.ended_at.strftime('%Y-%m-%d'),
-            "link": s_links,
-            "poster": poster_image_link
+            # "start_at" : anime.started_at.strftime('%Y-%m-%d'),
+            # "ended_at" : anime.ended_at.strftime('%Y-%m-%d'),
+            # "link": s_links,
+            # "poster": poster_image_link
         }
     return demo
 
-
-anime = 'Naruto'
-
-loop = asyncio.get_event_loop()
-loop.create_task(anime_search(str(anime)))
-# loop.run_until_complete(anime_search(str(anime)))
-data = loop.run_until_complete(asyncio.gather(anime_search(anime)))[0]
-# data = loop.run_until_complete(asyncio.as_completed(anime_search(anime)))
-print(data['Naruto'])
+# 
+# anime = 'Dragon Ball'
+# 
+# loop = asyncio.get_event_loop()
+# loop.create_task(anime_search(str(anime)))
+# # loop.run_until_complete(anime_search(str(anime)))
+# data = loop.run_until_complete(asyncio.gather(anime_search(anime)))[0]
+# # data = loop.run_until_complete(asyncio.as_completed(anime_search(anime)))
+# print(data['Dragon Ball'])
 # anime_search('Pokemon Introductory Recap')
-
-# for k,v in data.items():
+# 
+# # for k,v in data.items():
 #     print(k,v)
--- blog/views.py
+++ blog/views.py
@@ -20,14 +20,25 @@ async def kitsu(request, title):
     loop = asyncio.get_event_loop()
     loop.create_task(blog.models.anime_search(title))
     # print(blog.models.api_uses(title))
-    data = loop.run_until_complete(asyncio.gather(blog.models.anime_search(title)))
-    return HttpResponse(data)
+    data = loop.run_until_complete(asyncio.gather(blog.models.anime_search(title)))[0]
+    print(data)
+    return render(request, 'blog/show_detail.html', context={
+        "sub_type" : data[next(iter(data))]['sub-type'],
+        "status" : data[next(iter(data))]['status'],
+        "synopsis" : data[next(iter(data))]['synopsis'],
+        "episode" : data[next(iter(data))]['episode'],
+        "age_rating" : data[next(iter(data))]['age-rating'],
+        "popularity" : data[next(iter(data))]['popularity'],
+        "rating" : data[next(iter(data))]['rating'],
+        # "start_at" : data[next(iter(data))]['start_at'],
+        # "ended_at" : data[next(iter(data))]['ended_at']
+    })
 
 def test_kit():
     return blog.models.api_uses('naruto')
 
-def about(request):
-    return HttpResponse("This is a test of Django framework")
+# def about(request):
+#     return HttpResponse("This is a test of Django framework")
 
 
 def result_demo(request):
