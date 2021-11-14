f498a34df1560df80df56e83a8e059e9d12547be
boom210232 <chonchanok.che@ku.th>
2021-11-14 18:15:57 +0700

    change directory

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,17 @@ from django.urls import path
 
 import blog.views.search_view
 import blog.views.review_view
+import blog.views.about_us_view
 
 app_name = 'blog'
 
 urlpatterns = [
+
     path('', blog.views.search_view.index, name='index'),
     path('kitsu/<str:title>/', blog.views.search_view.kitsu, name='kitsu'),
     path('kitsu/search', blog.views.search_view.kitsu_search, name='search'),
     path('reviews/', blog.views.review_view.ReviewsView.as_view(), name='reviews'),
     path('create-review/', blog.views.review_view.CreateReviewView.as_view(success_url="/reviews"), name='create-review'),
+    path('contact/', blog.views.about_us_view.about_us, name='contact')
+
 ]
