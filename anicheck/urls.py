4ec0f2a3691f6c6ae69f076f977f5262d7c22b63
Ing140943 <skladeeing@gmail.com>
2021-11-17 21:57:52 +0700

    Complete Oauth google

--- anicheck/urls.py
+++ anicheck/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('signup/', views.signup, name='signup'),
 ]
