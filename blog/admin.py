f35872c8ec33854521db3af4df2264ed95bc222e
boom210232 <chonchanok.che@ku.th>
2021-11-25 19:41:28 +0700

    Update mylist

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from blog.models.blog import Review
+from blog.models.anime import Anime
 
 # Register your models here.
 admin.site.register(Review)
+admin.site.register(Anime)
--- blog/models/anime.py
+++ blog/models/anime.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Anime(models.Model):
-    title = models.CharField(blank=False)
-    url = models.CharField(blank=False)
+    title = models.TextField(blank=False)
+    url = models.TextField(blank=False),
     user = models.ForeignKey(User, on_delete=models.CASCADE)
