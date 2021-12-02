1ac20701080a8e1972758f79bf2376356e0a3649
boom210232 <chonchanok.che@ku.th>
2021-12-02 22:06:23 +0700

    add detail in docstring

--- blog/tests/test_deployment.py
+++ blog/tests/test_deployment.py
@@ -1,10 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 import requests
 
 
-class QuestionDetailViewTests(TestCase):
-    """Objective for test question is question can vote in properly time."""
-
+class DeploymentTests(TestCase):
+    """Objective for test deploy ment on heroku hosting"""
     def test_web_deploy(self):
         heroku_web = requests.get('http://anicheck-isp.herokuapp.com/')
         self.assertEqual(heroku_web.status_code, 200)
--- blog/tests/test_search.py
+++ blog/tests/test_search.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 
 nest_asyncio.apply()
 
-
-from django.test import TestCase, Client
+from django.test import TestCase
 import requests
+
 
 def make_dummy_url():
     original = str("http://anicheck-isp.herokuapp.com/kitsu/search?keyword=")
-    for i in range(0,5000):
+    for i in range(0, 5000):
         original += "f"
     return str(original)
 
 
-class QuestionDetailViewTests(TestCase):
-    """Objective for test question is question can vote in properly time."""
+class SearchTests(TestCase):
+    """Objective for test is how the result return when searching on deploy website."""
 
     def setUp(self):
         super().setUp()
@@ -26,7 +26,7 @@ class QuestionDetailViewTests(TestCase):
         self.user1.save()
 
     def test_search(self):
-        """Test that user can login via login view"""
+        """Test that user can search anime normally"""
         urls = requests.get("http://anicheck-isp.herokuapp.com/")
         # Can get to login page
         self.assertEqual(200, urls.status_code)
@@ -34,6 +34,7 @@ class QuestionDetailViewTests(TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_blank_search(self):
+        """Test that user can search for the blank line that not make crash or any error."""
         urls = requests.get("http://anicheck-isp.herokuapp.com/")
         # Can get to login page
         self.assertEqual(200, urls.status_code)
@@ -41,6 +42,6 @@ class QuestionDetailViewTests(TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_too_many_search(self):
+        """Test for show that user can not search for over than 4095 letters"""
         response = requests.get(make_dummy_url())
         self.assertEqual(400, response.status_code)
-
